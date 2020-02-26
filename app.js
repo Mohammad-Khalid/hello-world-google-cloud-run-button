520069220efa33b9a7278f891f1dfa7ecaf68ae6
unknown <mohdkhalid0807@gmail.com>
2020-02-26 15:03:44 +0530

    change port to 8080

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'),
       app = express(),
-      port = 3000;
+      port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
