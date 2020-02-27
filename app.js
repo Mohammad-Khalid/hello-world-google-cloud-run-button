7972ad87c05f513f9b5ddae70413c3e3812030df
unknown <mohdkhalid0807@gmail.com>
2020-02-27 17:24:01 +0530

    add console

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express'),
       client = require("./lib/mongoClient");
 
 app.get("/", async(req, res) => {
+
+    console.log("Request Coming into Express App...");
     const mongoClient = client.createGotClient();
-    await mongoClient.connect();
-    const item = await mongoClient.db("Test").collection('hello-world').find().toArray();
-    console.log(`item length: ${item.length}`);
-    res.send(`Hello World! I'm running on PORT : ${port}`);
+    try{
+
+        await mongoClient.connect();
+        const item = await mongoClient.db("Test").collection('hello-world').find().toArray();
+        console.log(`item length: ${item.length}`);
+        res.send(`Hello World! I'm running on PORT : ${port}`);
+    }catch(err){
+        res.send(err.message);
+    }
 });
 
 app.listen(port, () => {
